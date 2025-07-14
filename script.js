728bc8b60877a1aa43d382b25a86d0c8127d8bf5
Harivansh Mehta <harivanshmehta4@gmail.com>
2025-07-14 22:15:18 +0530

    Create operate function

--- script.js
+++ script.js
@@ -15,3 +15,20 @@ function divide(first, second) {
 }
 let num1 = 0, operator = '', num2 = 0;
 
+function operate(num1, operator, num2) {
+    switch (operator) {
+        case '+':
+            return add(num1, num2);
+            break;
+        case '-':
+            return subtract(num1, num2);
+            break;
+        case '*':
+            return multiply(num1, num2);
+            break;
+        case "/":
+            return divide(num1, num2);break;
+        default:
+            return 'Invalid operator'
+    }
+}
