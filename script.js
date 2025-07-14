d596c5db6625d7e048927decfa844b7f3504d988
Harivansh Mehta <harivanshmehta4@gmail.com>
2025-07-14 22:14:51 +0530

    Define global operation variables

--- script.js
+++ script.js
@@ -13,23 +13,5 @@ function multiply(first, second) {
 function divide(first, second) {
     return first / second;
 }
-
 let num1 = 0, operator = '', num2 = 0;
 
-function operate(num1, operator, num2) {
-    switch (operator) {
-        case '+':
-            return add(num1, num2);
-            break;
-        case '-':
-            return subtract(num1, num2);
-            break;
-        case '*':
-            return multiply(num1, num2);
-            break;
-        case "/":
-            return divide(num1, num2);break;
-        default:
-            return 'Invalid operator'
-    }
-}
